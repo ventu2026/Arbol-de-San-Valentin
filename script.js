00254b68fb9df3a9a12673c811d9ee4328cdf833
ventu2026 <animerandom2021@gmail.com>
2026-02-16 07:06:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@
     }));
 
     var textAnimate = eval(Jscex.compile("async", function () {
-    var together = new Date(2025, 9, 18, 14, 52, 0);
+    var together = new Date(2025, 9, 18, 4, 52, 0);
 
     $("#code").show().typewriter();
     $("#clock-box").fadeIn(500);
